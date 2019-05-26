816a891b760e582eb8edd3d2344c8d0103ce88ab
rafaquadros <rafael.quadros@quadrosti.com.br>
2019-05-26 00:32:14 +0000

    W2U3

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/base/Log"
+	"sap/base/Log",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 
-], function (Controller, Log) {
+], function (Controller, Log, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
+
+		formatter: formatter,
+
 		onInit: function () {
 			Log.info("Controller has been initialized.");
 		},
@@ -24,7 +30,29 @@ sap.ui.define([
 
 		onPress: function (sValue) {
 			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
-				oMessage.show("Searching..." + sValue);
+				var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+				oMessage.show(oResourceBundle.getText("search") + sValue);
+			}.bind(this));
+
+			var sCity = this.byId('city').getValue(),
+				sGenre = this.byId('genre').getSelectedItem().getKey(),
+				oCalendar = this.byId("calendar"),
+				oRowBinding = oCalendar.getBinding("rows"),
+				oFilterGenre,
+				oFilterCity;
+
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+
+			// Apply genre filter to calendar rows
+			oRowBinding.filter(oFilterGenre);
+
+			// Apply city filter to row appointments
+			var aRows = oCalendar.getAggregation("rows");
+			aRows.forEach(function (oItem) {
+				var oAppointmentsBinding = oItem.getBinding("appointments");
+				oAppointmentsBinding.filter(oFilterCity);
 			});
 		}
 
